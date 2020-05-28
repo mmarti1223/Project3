dea63622522debd86936b1b991f9cb6270cb6f4a
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2020-05-27 23:07:22 -0400

    added animal imagesa and fixed the grid

--- client/src/components/common/AnimalGrid.js
+++ client/src/components/common/AnimalGrid.js
@@ -6,14 +6,12 @@ import Row from "react-bootstrap/Row";
 export default class AnimalGrid extends Component {
   state = {
     animals: [
-      "https://www.nationalgeographic.com/content/dam/animals/2020/05/lion-evolution/01-lion-evolution-nationalgeographic_2711038.adapt.885.1.jpg",
-      "https://www.nationalgeographic.com/content/dam/animals/2020/05/lion-evolution/01-lion-evolution-nationalgeographic_2711038.adapt.885.1.jpg",
-      "https://www.nationalgeographic.com/content/dam/animals/2020/05/lion-evolution/01-lion-evolution-nationalgeographic_2711038.adapt.885.1.jpg",
-      "https://www.nationalgeographic.com/content/dam/animals/2020/05/lion-evolution/01-lion-evolution-nationalgeographic_2711038.adapt.885.1.jpg",
-      "https://www.nationalgeographic.com/content/dam/animals/2020/05/lion-evolution/01-lion-evolution-nationalgeographic_2711038.adapt.885.1.jpg",
-      "https://www.nationalgeographic.com/content/dam/animals/2020/05/lion-evolution/01-lion-evolution-nationalgeographic_2711038.adapt.885.1.jpg",
-      "https://www.nationalgeographic.com/content/dam/animals/2020/05/lion-evolution/01-lion-evolution-nationalgeographic_2711038.adapt.885.1.jpg",
-      "https://www.nationalgeographic.com/content/dam/animals/2020/05/lion-evolution/01-lion-evolution-nationalgeographic_2711038.adapt.885.1.jpg",
+      "https://ucarecdn.com/54e453ed-16a8-4f76-90e4-130c183ac5cd/basilisk_btn.jpg",
+      "https://ucarecdn.com/e7a117c0-a1bb-4d26-88b5-0e40b9a6701d/parrot_btn.jpg",
+      "https://ucarecdn.com/78183a8d-c3d7-434f-bc53-8976a09d035d/elephant_btn.jpg",
+      "https://ucarecdn.com/e8292661-536a-4809-b1ad-78ed29a8a176/whitetiger_btn.jpg",
+      "https://ucarecdn.com/7997ebb6-d302-4d14-ac7a-629437df93b8/gorilla_btn.jpg",
+      "https://ucarecdn.com/3a6089f2-ebb6-48e8-9cd1-8f76a315ae61/whitecheekedgibbon_btn.jpg",
     ],
   };
 
