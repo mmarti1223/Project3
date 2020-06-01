d7078a31404a28fa7079bc47f351845197942c1f
Enid <enid.soto@gmail.com>
2020-06-01 19:20:12 -0400

    added database seeds

---
+++ models/db/triviaSeeds.sql
@@ -0,0 +1,7 @@
+/* Add animal_id before feeding */
+INSERT INTO Trivia (question, answer, wrongAnswer, points, animal_id) VALUES ('Tigers live in the desert', false, true, 20, 1);
+INSERT INTO Trivia (question, answer, wrongAnswer, points, animal_id) VALUES ('Gorillas live alone', false, true, 20, 2);
+INSERT INTO Trivia (question, answer, wrongAnswer, points, animal_id) VALUES ('Female lions are hunters', true, false, 20, 3);
+INSERT INTO Trivia (question, answer, wrongAnswer, points, animal_id) VALUES ('Eyelash vipers eat frogs', true, false, 20, 4);
+INSERT INTO Trivia (question, answer, wrongAnswer, points, animal_id) VALUES ('Koalas live in China', false, true, 20, 5);
+INSERT INTO Trivia (question, answer, wrongAnswer, points, animal_id) VALUES ('Alligators lay eggs', true, false, 20, 6);
