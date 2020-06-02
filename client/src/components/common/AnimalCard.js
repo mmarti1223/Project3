25e500dc3b10b55b1a291447218ea6e010149521
Enid <enid.soto@gmail.com>
2020-06-01 21:37:29 -0400

    worked on animalCard and animalGrid components

--- client/src/components/common/AnimalCard.js
+++ client/src/components/common/AnimalCard.js
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 // import Col from "react-bootstrap/Col";
 
 export default class AnimalCard extends Component {
+  
   render() {
     return (
       <div className="animal-card">
-        <Link to="/animal-page/:id">
-          <img src={this.props.src} alt={this.props.src} width="100%" />
+        <Link to={`/animal-page/${this.props.id}`}>
+          <img src={this.props.src} alt={this.props.name} width="100%" />
         </Link>
       </div>
     );
