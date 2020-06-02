c8642bd975deb2340157f60aae27ff4f08057a8c
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2020-06-01 20:07:09 -0400

    Updated animal schema and seeds file

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("*", (req, res) => {
 });
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({}).then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
