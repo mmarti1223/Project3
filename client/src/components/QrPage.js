50ff4e39003a5f576a174deaf695f51d9bc32a25
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2020-06-13 13:54:50 -0400

    Added QR scanner functionality

--- client/src/components/QrPage.js
+++ client/src/components/QrPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { GlobalContext } from "../store";
 
 import Navbar from "./Navbar";
@@ -15,11 +15,11 @@ export default class QrPage extends Component {
             <Navbar />
             <Container className="text-center">
                 <h3 className="mt-5">Did you find the</h3>
-                <h2 className="nunito-font font-blue">[animal.name]</h2>
+                <h2 className="nunito-font font-blue">animal</h2>
                 <h3>in the zoo?</h3>
                 <img src={QrIllustration} alt="scan the qr code in the zoo" width="50%" className="my-5" />
                 <h2 className="font-blue worksans-light">Scan the code with your camera to play!</h2>
-                <button className="nunito-font red-bg my-5">Scan now</button>
+                <button className="nunito-font red-bg my-5"><Link to="/scanner">Scan now</Link></button>
             </Container>
         </div>
       );
