79db67fee596c99da6c071b3be10f1dcef5a4f5c
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2020-06-13 10:05:13 -0400

    worked on the redeem page

--- client/src/components/Redeem.js
+++ client/src/components/Redeem.js
@@ -2,7 +2,29 @@ import React from "react";
 import "../assets/css/redeem.css";
 
 const Redeem = () => {
-  return <div className="redeem">redeem</div>;
+  return (
+    <div className="redeem">
+      <img
+        className="coins"
+        src={require("../assets/images/coins_red.svg")}
+        alt="drink"
+      />
+      <h2 className="font-red">Yay!</h2>
+      <p>You have traded 50 points for</p>
+
+      <img
+        className="item"
+        src={require("../assets/images/soda.jpg")}
+        alt="drink"
+      />
+      <h3 className="font-red">Large Fountain Drink</h3>
+
+      <div className="bottom">
+        <p>Show this screen at the nearest food kiosk to get your prize.</p>
+        <h4>Redemption code: 23LFD20</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Redeem;
